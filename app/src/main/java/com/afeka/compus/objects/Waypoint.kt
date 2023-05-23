97c395efe8268cb879864902e99acbd415549918
liranbr <liranbraverman@gmail.com>
2023-05-23 18:53:01 +0300

    added pos to Waypoint

--- app/src/main/java/com/afeka/compus/objects/Waypoint.kt
+++ app/src/main/java/com/afeka/compus/objects/Waypoint.kt
@@ -5,14 +5,16 @@ import com.google.gson.annotations.SerializedName
 class Waypoint(
     @SerializedName("id") private val id: String,
     @SerializedName("place_id") private val placeId: String,
-    @SerializedName("area_id") private val areaId: String) {
+    @SerializedName("area_id") private val areaId: String,
+    @SerializedName("pos") private val pos: Map<String, Int>) {
 
     fun getId() = id
     fun getPlaceId() = placeId
     fun getAreaId() = areaId
+    fun getPos() = pos
 
     override fun toString(): String {
-        return "Waypoint: id=$id, place=$placeId, area=$areaId"
+        return "Waypoint: id=$id, place=$placeId, area=$areaId, pos=$pos"
     }
 
 }
